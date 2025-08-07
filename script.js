77f1c448249849b391455add4d2b298061213fc0
shreyansh-thakur <shreyanshthakur97@gmail.com>
2025-08-07 22:00:27 +0530

    reCAPTHA validation added

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-function beforesubmit(){
-    let outputdate = document.querySelector(".outputdate");
-    let inputdate = document.querySelector(".inputdate");
-    console.log("inputdate value: ", inputdate.value); //string --> date (en_IN)
+let capthaChecked = false;
+function beforesubmit(event){
+    if(capthaChecked){
+        let outputdate = document.querySelector(".outputdate");
+        let inputdate = document.querySelector(".inputdate");
+        console.log("inputdate value: ", inputdate.value); //string --> date (en_IN)
 
-    let formattedDate= new Date(inputdate.value).toLocaleDateString("en-IN");
-    outputdate.value = formattedDate;
+        let formattedDate= new Date(inputdate.value).toLocaleDateString("en-IN");
+        outputdate.value = formattedDate;
+    }
+    else{
+        alert("Please check the reCAPTHA box to submit the lead");
+        event.preventDefault();
+    }
 }
 
 function timestamp() { 
@@ -16,3 +23,7 @@ function timestamp() {
     } 
 } 
 setInterval(timestamp, 500);
+
+function capthaSuccess(){
+    capthaChecked= true;
+}
